5e6109a08d562a3537c7907c5598eec4f2f70650
clebronwrx <chris.lebron20@gmail.com>
2020-07-20 18:54:08 -0400

    removed wait

--- src/main/java/com/qa/subispeed/pages/HomePage.java
+++ src/main/java/com/qa/subispeed/pages/HomePage.java
@@ -3,8 +3,7 @@ package com.qa.subispeed.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import com.qa.subispeed.base.TestBase;
 
@@ -17,8 +16,8 @@ public class HomePage extends TestBase {
 	WebElement wrx;
 
 	public void clickWRXLink() {
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions.elementToBeClickable(wrx));
+//		WebDriverWait wait = new WebDriverWait(driver, 10);
+//		wait.until(ExpectedConditions.elementToBeClickable(wrx));
 		wrx.click();
 	}
 
