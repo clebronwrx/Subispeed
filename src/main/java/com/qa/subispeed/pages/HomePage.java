204c936415fbc86edd71e83000a31540e70749bd
clebronwrx <chris.lebron20@gmail.com>
2020-07-21 12:56:32 -0400

    added wait

--- src/main/java/com/qa/subispeed/pages/HomePage.java
+++ src/main/java/com/qa/subispeed/pages/HomePage.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.subispeed.base.TestBase;
 
@@ -17,9 +18,9 @@ public class HomePage extends TestBase {
 	WebElement wrx;
 
 	public void clickWRXLink() {
-//		WebDriverWait wait = new WebDriverWait(driver, 10);
-//		wait.until(ExpectedConditions.elementToBeClickable(wrx));
-		WebElement wrxLink=driver.findElement(By.xpath("//span[contains(text(),'15+ WRX')]/parent:: a"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		//wait.until(ExpectedConditions.elementToBeClickable(wrx));
+		WebElement wrxLink=wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(),'15+ WRX')]/parent:: a"))));
 		wrxLink.click();
 	}
 
